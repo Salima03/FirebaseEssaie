653dc2d84987a527540ab2117b5af2ccc6fda232
Salima03 <salimaelqartit20@gmail.com>
2024-11-16 22:51:04 +0100

    Modification MainActivity et layout

--- app/src/main/java/com/example/firebaseessaie/MainActivity.java
+++ app/src/main/java/com/example/firebaseessaie/MainActivity.java
@@ -1,42 +1,27 @@
 package com.example.firebaseessaie;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private FloatingActionButton floatingActionButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Write a message to the database
-        /*FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, word");*/
-        floatingActionButton=(FloatingActionButton) findViewById(R.id.floatingActionButton);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        // Récupérer le bouton
+        Button myButton = findViewById(R.id.myButton);
+
+        // Ajouter un écouteur d'événements au bouton
+        myButton.setOnClickListener(new View.OnClickListener() {
             @Override
-                    public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this, AddQuoteActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                // Afficher un Toast lorsqu'on clique sur le bouton
+                Toast.makeText(MainActivity.this, "Bouton cliqué !", Toast.LENGTH_SHORT).show();
             }
-
-            });
-        }
-
-
+        });
     }
+}
